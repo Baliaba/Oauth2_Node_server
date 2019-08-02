fb2bd23e2e7bf8667b83431f63c31dcccb35912b
2Code <valdezebaliaba@gmail.com>
2019-08-02 10:19:09 +0200

    update server to POST token

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const btoa = require('btoa')
 const { ISSUER, CLIENT_ID, CLIENT_SECRET, DEFAULT_SCOPE, PREFIX, PORT } = process.env
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 const test = async (token) => {
     try {
@@ -25,31 +28,55 @@ const test = async (token) => {
         console.log(`Error: ${error.message}`)
     }
 }
-app.get(PREFIX + '/token',  (req, res) => {
-    // console.log(req.query.CLIENT_ID)
-    const requestToken = btoa(`${req.query.client_id}:${req.query.client_secret}`);
-    const token = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)
-    if (requestToken != token) {
-        //console.log(requestToken , "--->", token)
+app.post(PREFIX + '/token', async (req, res) => {
+    if (typeof req.body.client_id != "undefined" &&
+        typeof req.body.client_secret != "undefined" &&
+        req.body.scope != "undefined") {
+        // req params founds 
+        var client_id = req.body.client_id
+        var client_secret = req.body.client_secret
+        var scope = req.body.scope
+        var requestToken = btoa(`${client_id}:${client_secret}`);
+        var token = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)
+
+        if (requestToken != token) {
+            //console.log(requestToken , "--->", token)
+            res.setHeader('Content-Type', 'application/json')
+            res.status(401)
+            res.json({ "Error 401 ": "Unauthorized" })
+        } else {
+            test(token).then((authorization) => {
+                if (typeof authorization != "undefined") {
+                    //const [authType, token] = auth.split(' ')
+                    console.log("========", authorization)
+                    var [authType, access_token] = authorization.split(' ')
+                    var response = {
+                        "scope": scope,
+                        "expires_in": 3600,
+                        "token_type": authType,
+                        "access_token": access_token,
+                    }
+                    res.setHeader('Content-Type', 'application/json')
+                    res.status(200)
+                    res.json(response)
+                } else{
+                    var response =
+                    {
+                        "scope": scope,
+                        "Error": "Internal server error",
+                    }
+                    res.setHeader('Content-Type', 'application/json')
+                    res.status(500)
+                    res.json(response)
+                }
+
+            })
+        }
+    } else {
+        // client not found 
         res.setHeader('Content-Type', 'application/json')
         res.status(401)
         res.json({ "Error 401 ": "Unauthorized" })
-    } else {
-        test(token).then((authorization) => {
-            //const [authType, token] = auth.split(' ')
-            console.log(authorization)
-            res.setHeader('Content-Type', 'application/json')
-            res.status(200)
-            const [authType, access_token] = authorization.split(' ')
-            var response =
-            { 
-                 "scope" : DEFAULT_SCOPE,
-                 "expires_in": 3600,
-                 "token_type": authType,
-                 "access_token": access_token,
-            }
-            res.json(response)
-        })
     }
 
 })
