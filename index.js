083695596253761a6ff53be31ee3003153642e52
Hassan <valdezebaliaba@gmail.com>
2019-08-04 12:41:28 +0200

    update access

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 require('dotenv').config()
 const request = require('request-promise')
 const btoa = require('btoa')
-const { ISSUER, CLIENT_ID, CLIENT_SECRET, DEFAULT_SCOPE, PREFIX, PORT,DEFAULT_PORT } = process.env
+const {
+    ISSUER,
+    CLIENT_ID,
+    CLIENT_SECRET,
+    DEFAULT_SCOPE,
+    PREFIX,
+    PORT,
+    DEFAULT_PORT
+} = process.env
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // support encoded bodies
 
 let getToken = async (token) => {
     try {
-        const { token_type, access_token } = await request({
+        const {
+            token_type,
+            access_token
+        } = await request({
             uri: `${ISSUER}/v1/token`,
             json: true,
             method: 'POST',
@@ -36,13 +49,17 @@ app.post(PREFIX + '/token', async (req, res) => {
         var client_id = req.body.client_id
         var client_secret = req.body.client_secret
         var scope = req.body.scope
-        var requestToken = btoa(`${client_id}:${client_secret}`);
-        var token = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)
-        if (requestToken != token) {
+        if (client_id != CLIENT_ID ||
+            client_secret != CLIENT_SECRET ||
+            scope != DEFAULT_SCOPE) {
             res.setHeader('Content-Type', 'application/json')
             res.status(401)
-            res.json({ "Error 401 ": "Unauthorized" })
+            res.json({
+                "status_code": "401",
+                "error ": "Unauthorized"
+            })
         } else {
+            var token = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)
             getToken(token).then((authorization) => {
                 if (typeof authorization != "undefined") {
                     var [authType, access_token] = authorization.split(' ')
@@ -55,9 +72,8 @@ app.post(PREFIX + '/token', async (req, res) => {
                     res.setHeader('Content-Type', 'application/json')
                     res.status(200)
                     res.json(response)
-                } else{
-                    var response =
-                    {
+                } else {
+                    var response = {
                         "scope": scope,
                         "Error": "Internal server error",
                     }
@@ -71,11 +87,13 @@ app.post(PREFIX + '/token', async (req, res) => {
     } else {
         // client not found 
         res.setHeader('Content-Type', 'application/json')
-        res.status(401)
-        res.json({ "Error 401 ": "Unauthorized" })
+        res.status(404)
+        res.json({
+            "status_code": "404",
+            "error ": "Not Found"
+        })
     }
 
 })
 const port = PORT || DEFAULT_PORT
 app.listen(port, () => console.log(`Listening on port ${port}`))
-
